8413449bdf78f8b8d453f1ea181bf8543cb413e1
TANVISHARMA08 <tanvi8sh@gmail.com>
2021-06-25 11:09:16 +0530

    REST Request from AddressBook

---
+++ src/main/java/com/blz/AddressBookSpring/AddressBookSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.blz.AddressBookSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookSpringProjectApplication.class, args);
+	}
+
+}

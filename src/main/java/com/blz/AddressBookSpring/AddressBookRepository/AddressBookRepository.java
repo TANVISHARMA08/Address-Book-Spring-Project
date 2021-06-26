4cb1f26644ca526e76c655f6956b1fc440aefc38
TANVISHARMA08 <tanvi8sh@gmail.com>
2021-06-26 14:01:48 +0530

    using lombook for logging

--- src/main/java/com/blz/AddressBookSpring/AddressBookRepository/AddressBookRepository.java
+++ src/main/java/com/blz/AddressBookSpring/AddressBookRepository/AddressBookRepository.java
@@ -1,7 +1,7 @@
+  
 package com.blz.AddressBookSpring.AddressBookRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 
 import com.blz.AddressBookSpring.model.AddressBookData;
 
--- src/main/java/com/blz/AddressBookSpring/AddressBookSpringProjectApplication.java
+++ src/main/java/com/blz/AddressBookSpring/AddressBookSpringProjectApplication.java
@@ -2,13 +2,18 @@ package com.blz.AddressBookSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookSpringProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookSpringProjectApplication.class, args);
-		System.out.println("Project is Deploying Properly");
+		ApplicationContext context = SpringApplication.run(AddressBookSpringProjectApplication.class, args);
+	
+		log.info("AddressBook app started in {} environment",
+				  context.getEnvironment().getProperty("environment"));
 	}
-
 }
